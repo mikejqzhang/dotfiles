025c77ca7fe1052d36a432c310dabd8f076203eb
mikejqzhang <mikejqzhang@gmail.com>
2018-01-22 15:14:40 -0800

    .

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -11,5 +11,35 @@ alias l='ls'
 alias cp='cp -i'
 alias mv='mv -i'
 
-# 
+# Personal
 alias cl='clear'
+
+# Update dotfiles
+dfu() {
+    (
+        cd ~/.dotfiles && git pull --ff-only && ./install -q
+    )
+}
+
+# Go up [n] directories
+up()
+{
+    local cdir="$(pwd)"
+    if [[ "${1}" == "" ]]; then
+        cdir="$(dirname "${cdir}")"
+    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+        echo "Error: argument must be a number"
+    elif ! [[ "${1}" -gt "0" ]]; then
+        echo "Error: argument must be positive"
+    else
+        for ((i=0; i<${1}; i++)); do
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
+        done
+    fi
+    cd "${cdir}"
+}
