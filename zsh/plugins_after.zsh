de0a7b4d0f0d899cd6c1625804336e3f960112a1
mikejqzhang <mikejqzhang@gmail.com>
2018-12-12 14:09:59 -0800

    updating submodules

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -2,7 +2,7 @@
 
 # Syntax highlighting
 
-# source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # 
 # ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 # 
