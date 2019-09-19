1a9d65b47f19d31c0ffee751170d0eb60c1741b3
mikejqzhang <mikejqzhang@gmail.com>
2019-09-19 18:17:43 +0100

    unbabies

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -6,9 +6,15 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(eyebrowse-close-window-config-prompt t)
+ '(eyebrowse-keymap-prefix "")
+ '(eyebrowse-mode nil)
+ '(eyebrowse-mode-line-style (quote always))
+ '(eyebrowse-slot-format "%s: untitled")
+ '(eyebrowse-tagged-slot-format "%s: %t")
  '(package-selected-packages
    (quote
-    (powerline org-bullets diminish evil-org eyebrowse magit multi-term helm-projectile projectile spaceline-all-the-icons spaceline spacemacs dash spacemacs-theme evil-collection neotree neo-tree helm dired all-the-icons-dired all-the-icons auto-compile key-chord evil undo-tree which-key use-package general))))
+    (yaml-mode multiple-cursors exec-path-from-shell flyspell-correct-helm flyspell-correct flycheck company-jedi company helm-flx powerline org-bullets diminish evil-org eyebrowse magit multi-term helm-projectile projectile spaceline-all-the-icons spaceline spacemacs dash spacemacs-theme evil-collection neotree neo-tree helm dired all-the-icons-dired all-the-icons auto-compile key-chord evil undo-tree which-key use-package general))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
