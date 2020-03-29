e5f0d6294995776cdeccd1ddf165c2dfc550bd3e
mikejqzhang <mikejqzhang@gmail.com>
2020-03-29 12:19:30 -0700

    making alias mac-friendly

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,7 +3,8 @@
 # ----------------
 
 # Use colors in coreutils utilities output
-alias ls='ls --color=auto'
+# alias ls='ls --color=auto'
+alias ls='ls -G'
 alias grep='grep --color'
 
 # ls aliases
