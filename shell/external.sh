bd88679777ae084470ffcc29cebc46a8f8f22ab3
mikejqzhang <mikejqzhang@gmail.com>
2023-06-01 15:10:09 -0500

    tramp

--- shell/external.sh
+++ shell/external.sh
@@ -6,18 +6,5 @@ else
   echo "Unknown OSTYPE: $OSTYPE"
 fi
 
-# pip should only run if there is a virtualenv currently activated
-# Now just here to make sure pip doesn't by default work as to not install globally
-# My package installations are done almost entirely in conda environments now so this
-# is more or less now a hack
-# export PIP_REQUIRE_VIRTUALENV=false
-# alias pip_on='export PIP_REQUIRE_VIRTUALENV=false'
-# alias pip_off='export PIP_REQUIRE_VIRTUALENV=true'
-
-# Cache pip-installed packages to avoid re-downloading
-# export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-
-# TODO: Change pip stuff to be only ok in conda envs with:
-# if [ "$CONDA_PREFIX" = "" ]; then
-#   is bad
-# fi
+# Fix for TRAMP
+[ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
